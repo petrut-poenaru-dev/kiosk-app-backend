c6c4bbf0efbd85d4342eeb90a0be8260df3845d2
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2024-08-01 22:56:19 +0300

    add server + get products and bundle settings

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,29 @@
-// server
+import express from "express";
+import chalk from "chalk";
+import fs from "fs";
+
+const app = express();
+const PORT = 3333;
+
+app.use(express.json({ limit: "50mb" }));
+
+app.listen(PORT , () => {
+    console.log(
+        chalk.bgBlue("Server started: "),
+        chalk.blue(`Listening to port ${PORT}`)
+    );
+
+    app.get('/api/bundle', (req, res) => {
+        fs.readFile( __dirname +'bundle' +".json", 'utf8', function (err, data) {
+            res.send(data);
+            res.end( data );
+        });
+    });
+
+    app.get('/api/products', (req, res) => {
+        fs.readFile( __dirname +'products' +".json", 'utf8', function (err, data) {
+            res.send(data);
+            res.end( data );
+        });
+    });
+})
